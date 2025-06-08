fbddd60e12192bb708bbe016901b9c879ea911ce
marishka <145292129+klefaa@users.noreply.github.com>
2025-06-08 19:52:56 +0300

    Update par.cu

--- task3/par.cu
+++ task3/par.cu
@@ -23,14 +23,10 @@ int main(int argc, char* argv[]) {
     int threads = atoi(argv[2]);
     int blocks = (size + threads - 1) / threads;
 
-    float* h_a = (float*)malloc(size * sizeof(float));
-    float* h_b = (float*)malloc(size * sizeof(float));
-    for (int i = 0; i < size; i++) {
-        h_a[i] = rand() / (float)RAND_MAX;
-        h_b[i] = rand() / (float)RAND_MAX;
-    }
-
+    float *h_a = (float*)malloc(size * sizeof(float));
+    float *h_b = (float*)malloc(size * sizeof(float));
     float *d_a, *d_b, *d_add, *d_sub, *d_mul, *d_div;
+
     cudaMalloc(&d_a, size * sizeof(float));
     cudaMalloc(&d_b, size * sizeof(float));
     cudaMalloc(&d_add, size * sizeof(float));
@@ -38,18 +34,41 @@ int main(int argc, char* argv[]) {
     cudaMalloc(&d_mul, size * sizeof(float));
     cudaMalloc(&d_div, size * sizeof(float));
 
-    cudaMemcpy(d_a, h_a, size * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b, h_b, size * sizeof(float), cudaMemcpyHostToDevice);
+    float totalTime = 0.0f;
+    srand(time(NULL));
 
-    clock_t start = clock();
-    array_ops<<<blocks, threads>>>(d_a, d_b, d_add, d_sub, d_mul, d_div, size);
-    cudaDeviceSynchronize();
-    clock_t end = clock();
+    for (int iter = 0; iter < 100; iter++) {
+        // Генерация случайных данных
+        for (int i = 0; i < size; i++) {
+            h_a[i] = rand() / (float)RAND_MAX;
+            h_b[i] = rand() / (float)RAND_MAX;
+        }
 
-    printf("Операции с массивами завершены.\n");
-    printf("Время выполнения: %.4f секунд\n", (double)(end - start) / CLOCKS_PER_SEC);
+        cudaMemcpy(d_a, h_a, size * sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_b, h_b, size * sizeof(float), cudaMemcpyHostToDevice);
+
+        // Таймеры CUDA
+        cudaEvent_t start, stop;
+        cudaEventCreate(&start);
+        cudaEventCreate(&stop);
+
+        cudaEventRecord(start, 0);
+        array_ops<<<blocks, threads>>>(d_a, d_b, d_add, d_sub, d_mul, d_div, size);
+        cudaEventRecord(stop, 0);
+        cudaEventSynchronize(stop);
+
+        float elapsed;
+        cudaEventElapsedTime(&elapsed, start, stop);
+        totalTime += elapsed;
+
+        cudaEventDestroy(start);
+        cudaEventDestroy(stop);
+    }
+
+    printf("Среднее время выполнения на GPU за 100 запусков: %.4f мс\n", totalTime / 100.0f);
 
     cudaFree(d_a); cudaFree(d_b); cudaFree(d_add); cudaFree(d_sub); cudaFree(d_mul); cudaFree(d_div);
     free(h_a); free(h_b);
+
     return 0;
 }
